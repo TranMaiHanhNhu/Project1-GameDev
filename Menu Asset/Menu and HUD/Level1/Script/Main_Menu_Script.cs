591dcbfce52fa81f84594da837dbf597c9e8657c
Nguyên-Computing <huynhhainguyen0706@gmail.com>
2026-04-18 16:04:01 +0700

    Delete Menu Asset directory

--- Menu Asset/Menu and HUD/Level1/Script/Main_Menu_Script.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Main_Menu_Script : MonoBehaviour
-{
-    public void ReturnScence()
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Main_Menu");
-    }
-}
