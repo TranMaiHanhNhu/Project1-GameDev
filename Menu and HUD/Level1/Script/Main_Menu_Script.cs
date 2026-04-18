e7bb2572e6d3641fd7971545ca4c451de552fe19
Nguyên-Computing <huynhhainguyen0706@gmail.com>
2026-04-18 16:05:54 +0700

    Menu and Hud Update

---
+++ Menu and HUD/Level1/Script/Main_Menu_Script.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Main_Menu_Script : MonoBehaviour
+{
+    public void ReturnScence()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main_Menu");
+    }
+}
---
+++ Menu and HUD/Menu Asset/Scripts/Level2.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Level2 : MonoBehaviour
+{
+   public void ChangeScene1()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level2");
+    }
+
+   
+}
